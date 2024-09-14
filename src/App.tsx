16241f4dc57a9bda07a4fef72663908ed65e4c1d
anancodec <aafrida@wesleyan.edu>
2024-09-14 18:02:03 -0400

    show location long and lat

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,25 @@ import "./App.css";
 import { useQuery } from "convex/react";
 import { api } from "../convex/_generated/api";
 
+
 function App() {
-  const tasks = useQuery(api.tasks.get);
-  return (
-    <div className="App">
-      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
-    </div>
-  );
+ const tasks = useQuery(api.tasks.get);
+ const locations = useQuery(api.locations.get);
+ return (
+   <div className="App">
+     <h2>Tasks</h2>
+     {tasks?.map(({ _id, text }) => (
+       <div key={_id}>{text}</div>
+     ))}
+
+
+     <h2>Locations</h2>
+     {locations?.map(({ _id, longitude }) => (
+       <div key={_id}>{Number(longitude)}</div>
+     ))}
+   </div>
+ );
 }
 
-export default App;
 
+export default App;
